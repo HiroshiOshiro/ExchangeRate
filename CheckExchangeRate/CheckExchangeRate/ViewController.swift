245f3ce737d708eb36f9bfdb0c23a9198dcd33a1
hiroshi oshiro <ooshiro010@gmail.com>
2020-04-30 22:59:24 +0900

    [add]made project

---
+++ CheckExchangeRate/CheckExchangeRate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CheckExchangeRate
+//
+//  Created by hiroshi on 2020/04/30.
+//  Copyright © 2020 hiroshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
